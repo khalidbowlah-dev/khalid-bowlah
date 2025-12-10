3730d72fa8e077330c16252279325f734e93f3c6
KHALED BOWLLAH MOUSSA <khalidbowlah@gmail.com>
2025-12-10 22:21:18 +0300

    Implemented restaurant menu logic

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,1 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Restaurant {
+    private List<MenuItem> menu;
+
+    public Restaurant() {
+        this.menu = new ArrayList<>();
+    }
+
+    // Add a new item to the restaurant's menu
+    public void addMenuItem(MenuItem item) {
+        menu.add(item);
+    }
+
+    // Get the whole list of items
+    public List<MenuItem> getMenu() {
+        return menu;
+    }
+
+    // A helper to print the menu nicely
+    public void displayMenu() {
+        System.out.println("--- MENU ---");
+        for (MenuItem item : menu) {
+            System.out.println(item.toString());
+        }
+    }
+}
