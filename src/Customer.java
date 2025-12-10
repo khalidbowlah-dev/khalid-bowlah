ea55becea737b09e23a72ed921042861c3c537db
KHALED BOWLLAH MOUSSA <khalidbowlah@gmail.com>
2025-12-10 22:17:44 +0300

    Implemented Customer inheritance and cart logic

--- src/Customer.java
+++ src/Customer.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
+// "extends User" means Customer gets all the data from User automatically
+public class Customer extends User {
+
+    // The Cart: A list to hold the MenuItems we pick
+    private List<MenuItem> cart;
+
+    public Customer(String name, String id) {
+        // "super" calls the constructor of the User class
+        super(name, id);
+        this.cart = new ArrayList<>();
+    }
+
+    // Method to add food to the cart
+    public void addToCart(MenuItem item) {
+        cart.add(item);
+        System.out.println(item.getName() + " added to cart.");
+    }
+
+    // Method to see what is inside
+    public List<MenuItem> getCart() {
+        return cart;
+    }
+}
