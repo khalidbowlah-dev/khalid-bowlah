72cb33d97cc00381ab1fe85d16dceefa4ab11f31
KHALED BOWLLAH MOUSSA <khalidbowlah@gmail.com>
2025-12-12 14:11:53 +0300

    Added cashpayment subclass

---
+++ src/CashPayment.java
@@ -0,0 +1,19 @@
+public class CashPayment extends Payment {
+    private double cashTendered;
+
+    public CashPayment(double amount, double cashTendered) {
+        super(amount);
+        this.cashTendered = cashTendered;
+    }
+
+    @Override
+    public void processPayment() {
+        System.out.println("Payment Type: CASH");
+        if (cashTendered >= amount) {
+            System.out.println("Total: $" + amount);
+            System.out.println("Change: $" + (cashTendered - amount));
+        } else {
+            System.out.println("Error: Insufficient cash.");
+        }
+    }
+}
